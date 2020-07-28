a741c180d41086518c00da6960dd547992907899
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-07-28 20:35:30 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,7 +1,6 @@
 function forLoop(array) {
   for(let i = 0; i < 25; i++) {
-    if(i === 1) {array[i] = "I am 1 strange loop.";}
-    else {array[i] = `\"I am ${i} strange loops.\"`}
+    array[i] = `\"I am ${i} strange loop${i === 1 ? '' : 's'}.\"`
   }
 }
 function whileLoop(array) {}
