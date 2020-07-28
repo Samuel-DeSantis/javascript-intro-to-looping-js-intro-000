593eadb464af6d4ebd822b27064f6583345bce2c
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-07-28 19:58:53 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,24 @@
+let i = 0;
+
+function incrementVariable() {return i++}
+
+function forLoop(array) {
+  var new_array =[];
+  for(let i = 0; i < 25; i++) {
+    new_array[i] = (`\"I am ${i} strange loop${i === 0 ? '' : 's'}.\" `);
+  }
+  return new_array;
+}
+
+function whileLoop(n) {
+  while(n > 0) {
+    console.log(n);
+    n--;
+  }
+}
+
+function doWhileLoop(num) {
+  do {
+    console.log("I run once regardless.")
+  } while (num === 0 ? pass : incrementVariable() < 10)
+}
